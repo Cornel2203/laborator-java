bdba8802f0755022fb10c415a6c56de3c16f71bf
unknown <cornel.misica@student.unitbv.ro>
2023-10-26 18:01:07 +0300

    t

---
+++ laborator 1/src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+    public static void main(String[] args){
+        int month_days[]={31,28,31,30,31,30,31,31,30,31,30,31};
+        String[] months={"January","February","March","April","May","June","July","August","September","October","November","December"};
+        int suma=0;
+        for(int i=0;i<months.length;i++)
+        {
+            System.out.println("Luna "+months[i]+" are "+month_days[i]+"zile");
+             if(month_days[i]==31)
+                System.out.println("Luna "+months[i]+" are 31 de zile");
+            if(month_days[i]<31)
+                 System.out.println("Luna "+months[i]+" are "+month_days[i]+" zile");
+            if(month_days[i]<31)
+                suma=suma+month_days[i];
+        }
+        System.out.println("suma lunilor cu mai putin de 31 de zile este "+suma);
+    }
+}
+
